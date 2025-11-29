34955ec72e1503a8861e7bab0ee557c9ccee17b0
quantv <quantv.85.com>
2025-11-29 08:44:16 +0700

    Init Setup window

---
+++ DMR2020/View/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using System.Windows;
+using System.IO.Ports;
+using DMR2020.View;
+
+namespace DMR2020
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            LoadComPorts();
+        }
+
+        private void LoadComPorts()
+        {
+            var ports = SerialPort.GetPortNames()
+                                  .OrderBy(p => p)
+                                  .ToList();
+
+            if (ports.Count == 0)
+            {
+                CboComPorts.ItemsSource = new[] { "No COM ports" };
+                CboComPorts.SelectedIndex = 0;
+                CboComPorts.IsEnabled = false;
+                return;
+            }
+
+            CboComPorts.ItemsSource = ports;
+            CboComPorts.SelectedIndex = 0;
+        }
+
+        private void SetupMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            var win = new SetupWindow();
+            win.Owner = this; // để nó coi MainWindow là cha
+            win.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            win.ShowDialog(); // hoặc win.Show() nếu muốn non-modal
+        }
+
+        private void QuitMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ DMR2020/View/Setup.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace DMR2020.View
+{
+    public partial class SetupWindow : Window
+    {
+        public SetupWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
