80c0be2d1f2fb9a998d31b1f74da347ad440f954
quantv <quantv.85.com>
2025-11-29 13:23:03 +0700

    Refine màn hình Setup

--- DMR2020/View/SetupWindow.xaml.cs
+++ DMR2020/View/SetupWindow.xaml.cs
@@ -1,27 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace DMR2020.View
 {
-    /// <summary>
-    /// Interaction logic for SetupWindow.xaml
-    /// </summary>
     public partial class SetupWindow : Window
     {
+        public ObservableCollection<TestItem> Items { get; set; }
+
         public SetupWindow()
         {
             InitializeComponent();
+
+            Items = new ObservableCollection<TestItem>
+            {
+                new TestItem { TestName = "Tensile Test A", CreatedTime = DateTime.Now.AddMinutes(-15) },
+                new TestItem { TestName = "Compression Test B", CreatedTime = DateTime.Now }
+            };
+
+            LvTests.ItemsSource = Items;
         }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            var item = (sender as FrameworkElement)?.DataContext as TestItem;
+            if (item != null)
+            {
+                MessageBox.Show($"Edit: {item.TestName}");
+            }
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var item = (sender as FrameworkElement)?.DataContext as TestItem;
+            if (item != null)
+            {
+                Items.Remove(item);
+            }
+        }
+    }
+
+    public class TestItem
+    {
+        public string TestName { get; set; }
+        public DateTime CreatedTime { get; set; }
+
+        // Format yêu cầu: dd/MM/yyyy HH:mm:ss
+        public string CreatedTimeFormatted =>
+            CreatedTime.ToString("dd/MM/yyyy HH:mm:ss");
     }
 }
